8a580dcd3573c39b274f539cb837bc8fa491cf02
Daniel Garza Flores <danielgarzaf97@gmail.com>
2022-01-20 20:19:50 -0600

    Moved contents from treesitter.vim to treesitter.lua

--- init.vim
+++ init.vim
@@ -10,7 +10,7 @@ source $HOME\AppData\Local\nvim\mappings.vim
 " PlugAppData\Local
 source $HOME\AppData\Local\nvim\plug-config\coc.vim
 source $HOME\AppData\Local\nvim\plug-config\lualine.lua
-source $HOME\AppData\Local\nvim\plug-config\treesitter.vim
+source $HOME\AppData\Local\nvim\plug-config\treesitter.lua
 source $HOME\AppData\Local\nvim\plug-config\telescope.lua
 source $HOME\AppData\Local\nvim\plug-config\web-devicons.vim
 
--- plug-config/treesitter.lua
+++ plug-config/treesitter.lua
@@ -1,12 +1,10 @@
-" if !exists('g:loaded_nvim_treesitter')
-"     finish
-" endif
-
-lua <<EOF
 require'nvim-treesitter.configs'.setup {
     -- Modules and its options go here
     highlight = { enable = true },
     incremental_selection = { enable = true },
     textobjects = { enable = true }
 }
-EOF
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.glsl = {
+    used_by = {"conf", "vert", "frag"}
+}
