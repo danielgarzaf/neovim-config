e700ce984d12007d77de2add950ce9d4850a4ebe
Daniel Garza Flores <danielgarzaf97@gmail.com>
2022-02-06 11:56:02 -0600

    Minor changes to plug-configs

--- plug-config/treesitter.lua
+++ plug-config/treesitter.lua
@@ -4,7 +4,4 @@ require'nvim-treesitter.configs'.setup {
     incremental_selection = { enable = true },
     textobjects = { enable = true }
 }
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.glsl = {
-    used_by = {"conf", "vert", "frag"}
-}
+require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
