2538c7ef7aa120b824571186a0f35ac6af9c3e47
Daniel Garza Flores <GarzaDaniel3@JohnDeere.com>
2024-12-30 11:58:47 -0600

    Add simple tab navigation using <C-l> and <C-h>
    to move left/right respectively

--- init.lua
+++ init.lua
@@ -6,5 +6,9 @@ require("config.lazy")
 -- General settings
 require("config.settings")
 
+-- Tab navigation
+vim.keymap.set("n", "<C-h>", require("config.tabs.navigate").move_left)
+vim.keymap.set("n", "<C-l>", require("config.tabs.navigate").move_right)
+
 -- Keys and mappings
 vim.cmd("source " .. CONFIG .. "/mappings.vim")
