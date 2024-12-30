72c7acc9873d5c1a92f9127d850f1cb6ef6bed55
Daniel Garza <danielgarzaf97@gmail.com>
2024-12-30 08:36:03 -0600

    Load lazy first to avoid weird black screen

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 CONFIG = vim.fn.stdpath("config"):gsub("\\", "/")
 
+-- Lazy (Package Manager)
+require("config.lazy")
+
 -- General settings
 require("config.settings")
 
 -- Keys and mappings
 vim.cmd("source " .. CONFIG .. "/mappings.vim")
-
--- Lazy (Package Manager)
-require("config.lazy")
